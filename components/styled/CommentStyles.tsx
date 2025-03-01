d441fff6fdb23e8d3ce8596ac745d2c3b177d0d1
Víctor Quiñones <victhorq716@gmail.com>
2025-03-01 00:06:46 -0400

    [refactor] --Refactor home page layout and add styled components --Updated the home page to use styled components for layout and removed unnecessary elements.

---
+++ components/styled/CommentStyles.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 2rem;
+`
+
+export const Header = styled.header`
+  text-align: center;
+  margin-bottom: 2rem;
+`
+
+export const Title = styled.h1`
+  font-size: 2rem;
+  color: #f1356d;
+`
