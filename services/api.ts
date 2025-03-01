e88e24f33d119a07fd6767ebd2d7769b481afe2c
Víctor Quiñones <victhorq716@gmail.com>
2025-03-01 13:09:47 -0400

    [feat] --Add new comments API integration --Implemented addComment API call and integrated it into zustand store for state management.

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { Comment } from '@/types/comment'
+import { Comment, NewComment } from '@/types/comment'
 
 const API_URL = 'https://jsonplaceholder.typicode.com'
 
@@ -9,5 +9,10 @@ export const apiBase = axios.create({
 
 export const getCommnets = async (): Promise<Comment[]> => {
   const response = await apiBase.get('/comments')
+  return response.data
+}
+
+export const addComment = async (comment: NewComment): Promise<Comment> => {
+  const response = await apiBase.post('/comments', comment)
   return response.data
 }
--- store/commentStore.ts
+++ store/commentStore.ts
@@ -1,14 +1,16 @@
 'use client'
 
 import { create } from 'zustand'
-import { Comment } from '@/types/comment'
-import { getCommnets } from '@/services/api'
+import { Comment, NewComment } from '@/types/comment'
+import { addComment, getCommnets } from '@/services/api'
+import { error } from 'console'
 
 interface CommentState {
   comments: Comment[]
   isLoading: boolean
   error: string | null
   fetchComments: () => Promise<void>
+  addNewComment: (comment: NewComment) => Promise<void>
 }
 
 export const useCommentStore = create<CommentState>((set) => ({
@@ -24,6 +26,21 @@ export const useCommentStore = create<CommentState>((set) => ({
       set({
         error:
           error instanceof Error ? error.message : 'Failed to get comments',
+        isLoading: false,
+      })
+    }
+  },
+  addNewComment: async (newComment: NewComment) => {
+    set({ isLoading: true, error: null })
+    try {
+      const comment = await addComment(newComment)
+      set((state) => ({
+        comments: [comment, ...state.comments],
+        isLoading: false,
+      }))
+    } catch {
+      set({
+        error: error instanceof Error ? error.message : 'Failed to add comment',
         isLoading: false,
       })
     }
